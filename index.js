b95fdc8ac2f4a139b68222fd5a6ca1a3d34f22e0
Aldo Cortés <aacortes11@gmail.com>
2021-07-16 17:24:00 -0500

    Init express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const log = console.log
-const http = require('http').createServer()
+const app = require('express')
+const http = require('http').createServer(app)
 const io = require('socket.io')(http, { cors: {
     origin: "*",
     methods: ["GET", "POST"]
