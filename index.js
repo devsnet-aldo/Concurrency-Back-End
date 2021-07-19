7f0f57e71f101e448ffec5071fac4b1e88c07aa3
Aldo Cortés <aacortes11@gmail.com>
2021-07-19 09:28:35 -0500

    Updated express CORS

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const log = console.log
 const app = require('express')
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, { cors: {
     origin: "*",
