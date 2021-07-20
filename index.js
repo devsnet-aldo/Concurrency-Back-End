13ed6d11f4c840c6bf5219be072ecff2fea17e4d
Aldo Cortés <aacortes11@gmail.com>
2021-07-20 08:19:03 -0500

    Added custom function for CORS

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const log = console.log
 const app = require('express')()
-var cors = require('cors')
-
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, { cors: {
     origin: "*",
@@ -9,7 +7,23 @@ const io = require('socket.io')(http, { cors: {
   } })
 const port = 3000
 
-app.use(cors({origin: '*'}));
+function allowCrossDomain(req, res, next) {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  
+    var origin = req.headers.origin;
+    if (_.contains(app.get('allowed_origins'), origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+  
+    if (req.method === 'OPTIONS') {
+      res.send(200);
+    } else {
+      next();
+    }
+  }
+  
+
+app.use(allowCrossDomain);
 
 http.listen(port, () => log(`server listening on port: ${port}`))
 
