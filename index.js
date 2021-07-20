e2771c30133a9efa0a7d3533717f4be633afb6f0
Aldo Cortés <aacortes11@gmail.com>
2021-07-20 08:23:20 -0500

    Forcing headers

--- index.js
+++ index.js
@@ -7,23 +7,11 @@ const io = require('socket.io')(http, { cors: {
   } })
 const port = 3000
 
-function allowCrossDomain(req, res, next) {
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  
-    var origin = req.headers.origin;
-    if (_.contains(app.get('allowed_origins'), origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-  
-    if (req.method === 'OPTIONS') {
-      res.send(200);
-    } else {
-      next();
-    }
-  }
-  
-
-app.use(allowCrossDomain);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 http.listen(port, () => log(`server listening on port: ${port}`))
 
